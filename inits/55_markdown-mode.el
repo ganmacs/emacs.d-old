e7a542f1aba418c3dc2fabd92585c07732717866
ganmacs <ganmacs@gmail.com>
2014-12-17 11:24:53 +0900

    fix markdown misc

--- inits/55_markdown-mode.el
+++ inits/55_markdown-mode.el
@@ -1,17 +1,15 @@
-(require 'markdown-mode)
-
-(with-eval-after-load 'markdown-mode
-  (define-key markdown-mode-map (kbd "M-p") nil)
-  (define-key markdown-mode-map (kbd "M-n") nil))
-
-;; githubflavar-mode用
-(defadvice delete-trailing-whitespace (around disable-in-markdown activate)
-  (unless (eq major-mode 'markdown-mode)
-    ad-do-it))
-
 (defun marked-preview-file ()
   (interactive)
   (shell-command
    (format "open -a Marked\\ 2.app %s"
            (shell-quote-argument (buffer-file-name)))))
-(define-key markdown-mode-map (kbd "C-c C-c o") 'marked-preview-file)
+
+(with-eval-after-load 'markdown-mode
+  (define-key markdown-mode-map (kbd "M-p") nil)
+  (define-key markdown-mode-map (kbd "M-n") nil)
+  (define-key markdown-mode-map (kbd "C-c C-c o") 'marked-preview-file))
+
+;; githubflavar-mode用
+(defadvice delete-trailing-whitespace (around disable-in-markdown activate)
+  (unless (eq major-mode 'markdown-mode)
+    ad-do-it))
