5e011892237c3d9149ef7668f0fd53f7c39b3373
ganmacs <ganmacs@gmail.com>
2015-02-02 10:19:11 +0900

    remove unused function

--- inits/15_smartparens.el
+++ inits/15_smartparens.el
@@ -24,16 +24,3 @@
 
 (eval-after-load "rhtml-mode"
   '(progn (require 'smartparens-html)))
-
-;; Emacs 24.4 からなくなったので
-(unless (fboundp 'cua-replace-region)
-  (defun cua-replace-region ()
-    "Replace the active region with the character you type."
-    (interactive)
-    (let ((not-empty (and cua-delete-selection (cua-delete-region))))
-      (unless (eq this-original-command this-command)
-        (let ((overwrite-mode
-               (and overwrite-mode
-                    not-empty
-                    (not (eq this-original-command 'self-insert-command)))))
-          (cua--fallback))))))
