34201cb3ca993a7807aaf941553f5aa6dad5f7df
ganmacs <ganmacs@gmail.com>
2015-07-17 11:46:03 +0900

    Change color of autocomplte menu

--- inits/13_auto-complete.el
+++ inits/13_auto-complete.el
@@ -9,6 +9,23 @@
  '(ac-use-menu-map t)
  '(ac-ignore-case nil))
 
+
+(let ((bgc "#333") (color "#aaa"))
+  ;; complete menu
+  (set-face-foreground 'ac-candidate-face color)
+  (set-face-background 'ac-candidate-face bgc)
+
+  ;; selected complate menu
+  (set-face-foreground 'ac-selection-face "white")
+  (set-face-background 'ac-selection-face "#729fcf")
+
+  (set-face-foreground 'popup-summary-face color)
+  (set-face-background 'popup-summary-face bgc)
+
+  ;; document with auto complete menu
+  (set-face-background 'popup-tip-face "#555")
+  (set-face-foreground 'popup-tip-face "#eee"))
+
 (setq ac-comphist-file (concat user-emacs-directory "log/ac-comphist.dat"))
 (add-to-list 'ac-dictionary-directories (expand-file-name "~/.emacs.d/etc/ac-dict"))
 (add-to-list 'ac-sources 'ac-source-yasnippet)
