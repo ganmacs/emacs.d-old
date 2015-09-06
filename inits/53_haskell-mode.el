96e357f91e7962ad09854fcd3651365ae4126ba3
ganmacs <ganmacs@gmail.com>
2015-09-06 11:23:04 +0900

    Change Haskell mode config
    fbecause last config is not runnig without errors

--- inits/53_haskell-mode.el
+++ inits/53_haskell-mode.el
@@ -1,7 +1,12 @@
-(add-to-list 'exec-path (concat (getenv "HOME") "/.cabal/bin"))
-(add-to-list 'interpreter-mode-alist '("runhaskell" . haskell-mode))
+;;; 53_haskell-mode.el --- haskell mode setting
+;;
+;;; Code:
+;;; Commentary:
 
-(defun my-haskell-mode-hook ()
+;; (add-to-list 'exec-path (concat (getenv "HOME") "/.cabal/bin"))
+;; (add-to-list 'interpreter-mode-alist '("runhaskell" . haskell-mode))
+
+(defun my/haskell-mode-hook ()
   (ghc-init)
   (setq flycheck-checker 'haskell-ghc)
   (add-to-list 'ac-sources 'ac-source-ghc-mod)
@@ -9,6 +14,7 @@
   (turn-on-haskell-indentation)
   (define-key haskell-mode-map (kbd "M-n") 'forward-paragraph)
   (define-key haskell-mode-map (kbd "M-p") 'backward-paragraph)
-  (define-key haskell-mode-map (kbd "C-h") 'haskell-indentation-delete-backward-char))
+  ;; (define-key haskell-mode-map (kbd "C-h") 'haskell-indentation-delete-backward-char)
+  )
 
-(add-hook 'haskell-mode-hook 'my-haskell-mode-hook)
+(add-hook 'haskell-mode-hook 'my/haskell-mode-hook)
