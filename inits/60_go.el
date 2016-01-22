ac5e2ddb5e3d314a07d953cd751ee6d11e891475
ganmacs <ganmacs@gmail.com>
2016-01-22 13:57:01 +0900

    for go

--- inits/60_go.el
+++ inits/60_go.el
@@ -2,9 +2,33 @@
 ;;; Commentary:
 ;;; Code:
 
+(with-eval-after-load 'go-mode
+  (define-key go-mode-map (kbd "M-.") 'godef-jump))
+
+
 (require 'go-autocomplete)
-(ac-config-default)
+(defun my/golang-mode ()
+  "My go-lang mode"
+  (setq gofmt-command "goimports")
+  (ac-config-default))
+
+(add-hook 'go-mode-hook 'my/golang-mode)
 (add-hook 'go-mode-hook 'go-eldoc-setup)
 (add-hook 'before-save-hook 'gofmt-before-save)
+
+(defvar my/helm-go-source
+  '((name . "Helm Go")
+    (candidates . (lambda ()
+                    (cons "builtin" (go-packages))))
+    (action . (("Show document" . godoc)
+               ("Import package" . my/helm-go-import-add)))))
+
+(defun my/helm-go-import-add (candidate)
+  (dolist (package (helm-marked-candidates))
+    (go-import-add current-prefix-arg package)))
+
+(defun my/godoc ()
+  (interactive)
+  (helm :sources '(my/helm-go-source) :buffer "*helm go*"))
 
 ;;; 60_go.el ends here
