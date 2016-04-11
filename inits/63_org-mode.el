cb777a6418b85c65f21d9f5db299c58e84184778
ganmacs <ganmacs@gmail.com>
2016-04-11 22:41:46 +0900

    Add for org mode

--- inits/63_org-mode.el
+++ inits/63_org-mode.el
@@ -2,28 +2,6 @@
 ;;; Commentary:
 ;;; Code:
 
-(custom-set-variables
- '(org-directory "~/Dropbox/org")
- '(org-default-notes-file (concat org-directory "/notes.org"))
- '(org-startup-folded nil)
- '(org-return-follows-link t)
- '(org-log-done 'time)
- '(org-agenda-files (list org-directory))
- '(org-capture-templates
-   '(("t" "Task" entry (file (expand-file-name (concat org-directory "/task.org")))
-      "* TODO %?\n    %i\n    %a\n    %T")
-     ("n" "note" entry (file (expand-file-name (concat org-directory "/notes.org")))
-      "* %?\n    %i\n    %a\n    %T"))))
-
-(with-eval-after-load 'org-mode
-  (define-key org-mode-map (kbd "C-j") 'dabbrev-expand)
-  (define-key org-mode-map (kbd "M-h") 'backward-kill-word)
-  (define-key org-mode-map (kbd "C-,") 'undo-tree-redo)
-  (define-key org-mode-map (kbd "s-S-r") 'org-latex-export-to-pdf))
-
-(global-set-key (kbd "C-c r") 'org-capture)
-(global-set-key (kbd "C-c a") 'org-agenda)
-
 (require 'ox-latex)
 (with-eval-after-load 'ox-latex
   (setq org-latex-pdf-process '("ptex2pdf -l -ot '-synctex=1 -shell-escape' %f"))
@@ -35,5 +13,38 @@
                  ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
                  ("\\paragraph{%s}" . "\\paragraph*{%s}")
                  ("\\subparagraph{%s}" . "\\subparagraph*{%s}"))))
+
+(custom-set-variables
+ '(org-directory "~/Dropbox/org")
+ '(org-default-notes-file (concat org-directory "/notes.org"))
+ '(org-startup-folded nil)
+ '(org-return-follows-link t)
+ '(org-log-done 'time)
+ '(org-agenda-files (list org-directory))
+ '(org-capture-templates
+   '(("t" "Task" entry (file (expand-file-name (concat org-directory "/task.org")))
+      "* TODO %?\n    %i\n   %a\n    %T")
+     ("n" "note" entry (file (expand-file-name (concat org-directory "/notes.org")))
+      "* %?\n   %a\n    %T")
+     ("r" "reading" entry (file (expand-file-name (concat org-directory "/reading.org")))
+      "* %?\n   %a\n    %T"))))
+
+(defun my/org-mode-hook ()
+  "My org mode hook."
+  (define-key org-mode-map (kbd "C-j") 'dabbrev-expand)
+  (define-key org-mode-map (kbd "M-h") 'backward-kill-word)
+  (define-key org-mode-map (kbd "C-,") 'undo-tree-redo)
+  (define-key org-mode-map (kbd "s-S-r") 'org-latex-export-to-pdf))
+(add-hook 'org-mode-hook 'my/org-mode-hook)
+
+(global-set-key (kbd "C-c C-q") 'org-capture)
+(global-set-key (kbd "C-c a") 'org-agenda)
+
+(defun org/note-right-now (content)
+  (interactive "sContent: ")
+  (org-capture nil "n")
+  (insert content)
+  (org-capture-finalize))
+(global-set-key (kbd "C-M-=") 'org/note-right-now)
 
 ;;; 63_org-mode.el ends here
