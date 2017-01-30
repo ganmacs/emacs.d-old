22fd7bc124aee679d8a6587f2e840dae70eabcba
ganmacs <ganmacs@gmail.com>
2017-01-30 13:09:28 +0900

    Add rust config

--- inits/64_rust-mode.el
+++ inits/64_rust-mode.el
@@ -2,26 +2,23 @@
 ;;; Commentary:
 ;;; Code:
 
-(setq racer-rust-src-path "/Users/ganmacs/.rust/src/")
-(setq racer-cmd "/Users/ganmacs/.cargo/bin/racer")
+(add-to-list 'exec-path (expand-file-name "~/.cargo/bin/"))
+
+(defun my/rust-mode-hook ()
+  "My rust mode."
+  (racer-mode)
+  (flycheck-rust-setup)
+  (rust-enable-format-on-save))
+(add-hook 'rust-mode-hook 'my/rust-mode-hook)
 
 (with-eval-after-load 'racer-mode
+  (add-to-list 'jumplist-hook-commands 'racer-find-definition)
   (add-to-list 'company-backends 'company-racer))
-
-(defun rust/insert-semicoron ()
-  (interactive)
-  (save-excursion
-    (move-end-of-line 1)
-    (insert ";")))
 
 (defun my/racer-mode-hook ()
   "My racer mode."
-  (define-key racer-mode-map (kbd "M-/") 'company-complete)
-  (define-key racer-mode-map (kbd "s-i") 'rust/insert-semicoron))
-
-(add-hook 'rust-mode-hook #'racer-mode)
+  (define-key racer-mode-map (kbd "M-/") 'company-complete))
 (add-hook 'racer-mode-hook #'eldoc-mode)
 (add-hook 'racer-mode-hook 'my/racer-mode-hook)
-
 (add-hook 'flycheck-mode-hook #'flycheck-rust-setup)
 ;;; 64_rust-mode.el ends here
