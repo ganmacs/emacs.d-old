ba61bcdc951143c1cbcfbd8385a4c98e5b5f0d30
ganmacs <ganmacs@gmail.com>
2017-01-31 23:41:34 +0900

    Fix elixer mode

--- inits/61_elixir-mode.el
+++ inits/61_elixir-mode.el
@@ -2,14 +2,12 @@
 ;;; Commentary:
 ;;; Code:
 
-(require 'alchemist)
-
-
 (with-eval-after-load 'elixir-mode
-  (define-key elixir-mode-map (kbd "M-/") 'company-complete)
+ (require 'alchemist)
+ (define-key elixir-mode-map (kbd "M-/") 'company-complete)
   (define-key company-active-map (kbd "C-c") (lambda (x)
                                                (interactive)
                                                (alchemist-company--show-documentation x))))
 (add-hook 'elixir-mode-hook #'company-mode)
 
-;;; 61_elixir-mode.el ends here
+;; 61_elixir-mode.el ends here
