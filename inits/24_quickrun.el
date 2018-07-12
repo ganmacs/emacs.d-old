9b6d0f69f729387d1a38af2f351f7a71f1389717
ganmacs <ganmacs@gmail.com>
2018-07-12 13:07:18 +0900

    Quick run uses c++11

--- inits/24_quickrun.el
+++ inits/24_quickrun.el
@@ -9,6 +9,10 @@
       (quickrun-region (region-beginning)  (region-end))
     (quickrun)))
 
+(quickrun-add-command "c++/clang++"
+  '((:exec . ("%c -std=c++11 %o -o %e %s" "%e %a")))
+  :override t)
+
 (global-set-key (kbd "s-R") 'my/quickrun)
 
 ;;; 24_quickrun.el ends here
